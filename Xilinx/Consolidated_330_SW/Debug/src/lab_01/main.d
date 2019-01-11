36dcbcdb6ff793d9fb1aa426147a2ae728b04afb
Clint Reed Frandsen <clintrf@et.byu.edu>
2019-01-10 22:09:11 -0700

    new

--- Xilinx/Consolidated_330_SW/Debug/src/lab_01/main.d
+++ Xilinx/Consolidated_330_SW/Debug/src/lab_01/main.d
@@ -1,4 +1,7 @@
 src/lab_01/main.d: ../src/lab_01/main.c \
- J:\ECEN_390\Xilinx\Consolidated_330_SW/supportFiles/display.h
+ J:\ECEN_390\Xilinx\Consolidated_330_SW/supportFiles/display.h \
+ ../src/lab_01/queue.h
 
 J:\ECEN_390\Xilinx\Consolidated_330_SW/supportFiles/display.h:
+
+../src/lab_01/queue.h:
